242c2ae5b05f1de0c8e61ec1f3d5634420d021cb
Anton Forikov <tohabass@gmail.com>
2024-03-21 17:31:23 +0600

    backend: Added caesar-salad logic to corresponding endpoints

--- api/index.ts
+++ api/index.ts
@@ -1,19 +1,20 @@
 import express from 'express';
+import cors from 'cors';
 const Vigenere = require('caesar-salad').Vigenere;
 
 const app = express();
+app.use(express.json());
+app.use(cors({origin: ['http://localhost:5173']}))
 const port = 8000;
 
-// app.get('/', (req, res) => {
-//   return res.send('This is Home page.')
-// });
-
-app.post('/encode/:target', (req, res) => {
-  res.send(Vigenere.Cipher(password).crypt(req.params.target));
+app.post('/encode', (req, res) => {
+  const {password, message} = req.body;
+  return res.json({encoded: Vigenere.Cipher(password).crypt(message)});
 });
 
-app.post('/decode/:target', (req,res) => {
-  res.send(Vigenere.Decipher(password).crypt(req.params.target));
+app.post('/decode', (req,res) => {
+  const {password, message} = req.body;
+  return res.json({decoded: Vigenere.Decipher(password).crypt(message)});
 });
 
 app.listen(port, () => {
