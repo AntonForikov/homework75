15ab1e2311819c7cdd3e0cd2bcf2aaf54266fb3f
Anton Forikov <tohabass@gmail.com>
2024-03-21 17:32:48 +0600

    frontend: Added check whitespaces and empty strings to input fields and caching errors

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,27 +1,137 @@
-import {Grid, IconButton, TextField} from '@mui/material';
+import {CircularProgress, Grid, IconButton, TextField} from '@mui/material';
 import ArrowDownwardIcon from '@mui/icons-material/ArrowDownward';
 import ArrowUpwardIcon from '@mui/icons-material/ArrowUpward';
+import axios from 'axios';
+import React, {useState} from 'react';
+interface Message {
+  password: string
+  decodedMessage: string
+  encodedMessage: string
+}
+
+const initialMessage = {
+  password: '',
+  decodedMessage: '',
+  encodedMessage: ''
+}
+
+interface Encoded {
+  encoded: string
+}
+
+interface Decoded {
+  decoded: string
+}
 
 function App() {
+  const [message, setMessage] = useState<Message>(initialMessage);
+  const [loading, setLoading] = useState(false);
+
+  const changeMessage = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const {name, value} = e.target;
+
+    setMessage((prevState) => ({
+      ...prevState,
+      [name]: value
+    }));
+  };
+
+  const encodeMessage = async () => {
+    const toSend = {
+      message: message.decodedMessage,
+      password: message.password
+    };
+
+    if (message.decodedMessage[0] === ' ') {
+      alert("Decoded message can't start from whitespace.");
+    } else if (message.decodedMessage === '') {
+      alert("Decoded message can't be an empty string.");
+    } else if (message.password[0] === ' ') {
+      alert("Password can't start from whitespace.");
+    } else if (message.password === '') {
+      alert("Password can't be an empty string.");
+    } else {
+      try {
+        setLoading(true);
+        const {data} = await axios.post<Encoded>('http://localhost:8000/encode', toSend);
+        setMessage((prevState) => ({...prevState, encodedMessage: data.encoded}));
+        setLoading(false);
+      } catch (e) {
+        alert('Please check URL or run server!');
+        console.log(e);
+      }
+    }
+  };
+
+  const decodeMessage = async () => {
+    const toSend = {
+      message: message.encodedMessage,
+      password: message.password
+    };
+
+    if (message.encodedMessage[0] === ' ') {
+      alert("Encoded message can't start from whitespace.");
+    } else if (message.encodedMessage === '') {
+      alert("Encoded message can't be an empty string.");
+    } else if (message.password[0] === ' ') {
+      alert("Password can't start from whitespace.");
+    } else if (message.password === '') {
+      alert("Password can't be an empty string.");
+    } else {
+      try {
+        setLoading(true);
+        const {data} = await axios.post<Decoded>('http://localhost:8000/decode', toSend);
+        setMessage((prevState) => ({...prevState, decodedMessage: data.decoded}));
+        setLoading(false);
+      } catch (e) {
+        alert('Please check URL or run server!');
+        console.log(e);
+      }
+    }
+  };
 
   return (
-    <>
-      <Grid container direction="column" gap={2} sx={{maxWidth: 400, mt: 4}} margin='auto'>
-        <TextField multiline variant='outlined' label='Decoded message' rows={5} />
-        <Grid container justifyContent='space-between' alignItems='center'>
-          <TextField variant='outlined' label='Password' sx={{maxWidth: '78%'}} fullWidth/>
-          <Grid>
-            <IconButton>
-              <ArrowDownwardIcon />
-            </IconButton>
-            <IconButton>
-              <ArrowUpwardIcon />
-            </IconButton>
-          </Grid>
+    <Grid container direction="column" gap={2} sx={{maxWidth: 400, mt: 4}} margin="auto">
+      <TextField
+        multiline
+        variant="outlined"
+        label="Decoded message"
+        rows={5}
+        name="decodedMessage"
+        value={message.decodedMessage}
+        onChange={changeMessage}
+      />
+      <Grid container justifyContent="space-between" alignItems="center">
+        <TextField
+          variant="outlined"
+          label="Password"
+          type='password'
+          sx={{maxWidth: '78%'}}
+          fullWidth
+          name="password"
+          value={message.password}
+          onChange={changeMessage}
+        />
+        <Grid>
+          <IconButton type="button" disabled={loading} onClick={encodeMessage}>
+            <ArrowDownwardIcon/>
+          </IconButton>
+          <IconButton type="button" disabled={loading} onClick={decodeMessage}>
+            <ArrowUpwardIcon/>
+          </IconButton>
         </Grid>
-        <TextField multiline variant='outlined' label='Encoded message' rows={5} />
       </Grid>
-    </>
+      <TextField
+        multiline
+        variant="outlined"
+        label="Encoded message"
+        rows={5}
+        name="encodedMessage"
+        value={message.encodedMessage}
+        onChange={changeMessage}
+      />
+      {loading && <Grid alignSelf='center'><CircularProgress/></Grid>}
+    </Grid>
   );
 }
 
