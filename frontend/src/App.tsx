6dc216a04bc44bfbe6770141e2bb8cd9ca5ef7b3
Anton Forikov <tohabass@gmail.com>
2024-03-21 13:40:05 +0600

    refactor: styles

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,6 @@
-import {Grid, TextField} from '@mui/material';
+import {Grid, IconButton, TextField} from '@mui/material';
+import ArrowDownwardIcon from '@mui/icons-material/ArrowDownward';
+import ArrowUpwardIcon from '@mui/icons-material/ArrowUpward';
 
 function App() {
 
@@ -6,7 +8,17 @@ function App() {
     <>
       <Grid container direction="column" gap={2} sx={{maxWidth: 400, mt: 4}} margin='auto'>
         <TextField multiline variant='outlined' label='Decoded message' rows={5} />
-        <TextField variant='outlined' label='Password'/>
+        <Grid container justifyContent='space-between' alignItems='center'>
+          <TextField variant='outlined' label='Password' sx={{maxWidth: '78%'}} fullWidth/>
+          <Grid>
+            <IconButton>
+              <ArrowDownwardIcon />
+            </IconButton>
+            <IconButton>
+              <ArrowUpwardIcon />
+            </IconButton>
+          </Grid>
+        </Grid>
         <TextField multiline variant='outlined' label='Encoded message' rows={5} />
       </Grid>
     </>
